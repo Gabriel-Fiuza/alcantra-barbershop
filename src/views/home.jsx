9ae0bfbcd779fb42d6097b80a9369af4915d5f55
Gabriel Fiuza <gpaulinellifiuza@gmail.com>
2025-07-10 18:07:45 -0300

    images added, responsiveness fixed, first page completed(?)

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,15 +1,12 @@
 import '../styles/home.css'
 import Navigation from '../components/Navigation'
-import Service from '../components/Service'
 import logo from '../assets/logo-preto.jpeg'
-import about from '../assets/aboutus.jpeg'
-import agende1 from '../assets/AGENDE_01.png'
-import agende2 from '../assets/AGENDE_02.png'
-import agende3 from '../assets/AGENDE_03.png'
-import agende4 from '../assets/AGENDE_04.png'
-import horario1 from '../assets/FUNCIONAMENTO_01.png'
-import horario2 from '../assets/FUNCIONAMENTO_02.png'
+import about from '../assets/aboutus.png'
+import banner1 from '../assets/bannerApp.png'
+import banner2 from '../assets/bannerApp2.png'
+import funcionamento from '../assets/funcionamento.png'
 import onlyBarra from '../assets/footerimg.jpeg'
+import servicesMenu from '../assets/services.png'
 
 export default function Home(){
     return(
@@ -26,13 +23,11 @@ export default function Home(){
             <div className="about">
                 <div className="aboutus">
                     <img src={about} alt="foto-da-barbearia" />
-                    <p>Com uma pegada contemporânea e foco total na experiência do cliente, a Alcantra Barber chegou pra mostrar que barbearia também é sobre conexão, estilo e identidade. Aqui, o corte é só o começo.
-                    Criada em 2022, e localizada na Praça Rotary, 945 — Bairro Monsenhor Parreiras, Luz/MG, a Alcantra Barber vem conquistando espaço com profissionalismo, qualidade nos serviços e um ambiente que mistura modernidade e autenticidade.
-                    Nossa missão é simples: fazer com que cada cliente se sinta em casa, saia satisfeito e volte não só pelo corte, mas pelo respeito, pelo papo e pela vibe única que rola por aqui.</p>
                 </div>
-                <h2 className="about-write"> NOSSOS SERVIÇOS:</h2>
+                {/* <h2 className="about-write"> NOSSOS SERVIÇOS:</h2> */}
                 <div className="services">
-                    <Service title='CORTE' price='30,00'/>
+                    <img src={servicesMenu} className='img-service' alt="tabela de serviços" />
+                    {/* <Service title='CORTE' price='30,00'/>
                     <Service title='BARBOTERAPIA' price='30,00'/>
                     <Service title='PEZINHO' price='10,00'/>
                     <Service title='SELAGEM' price='70,00'/>
@@ -44,10 +39,7 @@ export default function Home(){
                     <Service title='TINTURA CABELO' price='30,00'/>
                     <Service title='TINTURA BARBA' price='20,00'/>
                     <Service title='HIDRATAÇÃO CABELO OU BARBA' price='25,00'/>
-                    <Service title='CONSULTORIA VISAGISTA' price='150,00'/>
-                </div>
-                <div className="download">
-                    <p>Agende agora mesmo pelo app:</p>
+                    <Service title='CONSULTORIA VISAGISTA' price='150,00'/> */}
                     <div className="icons">
                         <a href="https://apps.apple.com/br/app/alcantra-barbearia/id6743761947"><button><img  src="https://img.icons8.com/fluency/48/apple-app-store.png" alt="apple-app-store"/>App store</button></a>
                         <a href="https://play.google.com/store/apps/details?id=alcantrabarbearia.bestbarbers.app"><button><img  src="https://img.icons8.com/fluency/48/google-play-store-new.png" alt="google-play-store-new"/>Playstore</button></a>    
@@ -55,27 +47,26 @@ export default function Home(){
                 </div>
             </div>
             <div className="feedbacks">
-                <img src={agende1} alt="" />
-                <img src={agende2} alt="" />
-                <img src={agende3} alt="" />
-                <img src={agende4} alt="" />
+                <img src={banner1} alt="" />
+                <img src={banner2} alt="" />
             </div>
-            <div className="local-hours-content">
+            {/* <div className="local-hours-content"> */}
                 <div className="hours">
-                    <img src={horario1} alt="horário de funcionamento"/>
-                    <img src={horario2} alt="horário de funcionamento" />
+                    <img src={funcionamento} alt="horário de funcionamento"/>
+                    <button>Abrir com o Google Maps</button>
                 </div>
                 <div className="location">
-                    <iframe
+                    {/* <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1379.6401582235949!2d-45.683070574026964!3d-19.78994165606067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b3a5555b851f1f%3A0x218473d39e0bc24a!2sAlcantra%20barber!5e0!3m2!1spt-BR!2sbr!4v1744842730139!5m2!1spt-BR!2sbr"
-                        style={{ border: 0, borderRadius: '12px' }}
+                        style={{ border: 0 }}
                         allowFullScreen
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                         title="Localização Alcantra Barber">
-                    </iframe>
+                    </iframe> */}
+                    {/* <img src={local1} alt="" /> */}
                 </div>
-            </div>
+            {/* </div> */}
             <footer>
                 <div className="footer">
                     <img className='logo-barra' src={onlyBarra} alt="" />
